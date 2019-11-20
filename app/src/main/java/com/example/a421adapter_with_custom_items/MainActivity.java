064b3319efe4072dc0dde1214897fe4273dca1c8
denlog1977 <denlog1977@gmail.com>
2019-11-20 16:07:18 +0300

    chuvixa200

--- app/src/main/java/com/example/a421adapter_with_custom_items/MainActivity.java
+++ app/src/main/java/com/example/a421adapter_with_custom_items/MainActivity.java
@@ -1,20 +1,15 @@
 package com.example.a421adapter_with_custom_items;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
